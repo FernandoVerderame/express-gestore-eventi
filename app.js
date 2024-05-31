0eacc30a249171ceb86057cf92e17d3f59f3b33f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-31 09:43:29 +0200

    started server and added base route

--- app.js
+++ app.js
@@ -7,5 +7,17 @@ const express = require('express');
 // Inizializzo express
 const app = express();
 
-// Definisco le variabili port
+// Definisco le variabili port e host
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+// Rotta base
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, './welcome.html');
+    res.sendFile(filePath);
+});
+
+// Avvio il server
+app.listen(port, host, () => {
+    console.log(`Server avviato su http://${host}:${port}`);
+});
