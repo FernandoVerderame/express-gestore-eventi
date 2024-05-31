f0d3759b07ea2563a5321f79f19a1b254b9d516e
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-31 09:37:00 +0200

    app setup

---
+++ app.js
@@ -0,0 +1,11 @@
+// Importo il path
+const path = require("path")
+
+// Importo express
+const express = require('express');
+
+// Inizializzo express
+const app = express();
+
+// Definisco le variabili port
+const port = process.env.PORT || 3000;
