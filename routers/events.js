d0649005561797963c92270540c1af76959ed15c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-31 09:52:39 +0200

    created events router and imported express

---
+++ routers/events.js
@@ -0,0 +1,8 @@
+// Importo express
+const express = require('express');
+
+// Istanza di express.Router()
+const router = express.Router();
+
+// Esporto l'istanza di router
+module.exports = router;
