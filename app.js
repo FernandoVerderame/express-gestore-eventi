fac5526dcfa30e7e69db74c611c84761e033cb99
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-31 09:53:15 +0200

    added events routes

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+// Router degli eventi
+const eventsRouter = require("./routers/events.js");
+
+// Indico ad express che esistono nuove rotte per gli eventi
+app.use('/events', eventsRouter);
+
 // Rotta base
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, './welcome.html');
