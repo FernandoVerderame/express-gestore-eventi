79eebb016e56f80ca4463107ab52576c208daa72
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-31 12:19:20 +0200

    added readJSON, writeJSON and addEvent static function

--- models/events.js
+++ models/events.js
@@ -1,3 +1,12 @@
+// Importo il modulo File System
+const fs = require("fs");
+
+// Importo il modulo Path
+const path = require("path");
+
+// Importo il modulo Crypto
+const crypto = require("crypto");
+
 class Event {
 
     constructor(title, description, date, maxSeats) {
@@ -11,5 +20,26 @@ class Event {
     generateId() {
         const id = crypto.randomUUID();
         return id;
+    }
+
+    // Lettura file
+    static readJSON() {
+        const filePath = path.join(__dirname, "../db/events.json");
+        const fileData = fs.readFileSync(filePath);
+        return JSON.parse(fileData);
+    }
+
+    // Scrittura file
+    static writeJSON(newData) {
+        const filePath = path.join(__dirname, "../db/events.json");
+        const fileString = JSON.stringify(newData, null, 2);
+        fs.writeFileSync(filePath, fileString);
+    }
+
+    // Aggiungi un nuovo evento
+    static addEvent(event) {
+        const events = this.readJSON();
+        events.push(event);
+        this.writeJSON(events);
     }
 }
