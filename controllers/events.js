b410f0a272cd75460dad4886cacb11dd8006718e
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-31 13:16:48 +0200

    added query string filter into index

--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,19 @@
+// Importo la classe Event
+const eventModel = require("../models/events.js");
+
 // Index
 const index = (req, res) => {
 
+    if (req.query.id) {
+        // Recupero l'Evento tramite ID se c'è in query string
+        const id = req.query.id;
+        const event = eventModel.getEventId(id);
+        res.json(event);
+    } else {
+        // Recupero tutti gli Eventi
+        const events = eventModel.readJSON();
+        res.json(events);
+    }
 }
 
 // Store
